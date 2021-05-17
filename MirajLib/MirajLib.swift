a6e471d3bd128e6b0985f02de4ec3ba438cb5288
miraj <miraj@iroidsolutions.com>
2021-05-17 12:00:57 +0530

    Test add

---
+++ MirajLib/MirajLib.swift
@@ -0,0 +1,23 @@
+//
+//  MirajLib.swift
+//  MirajLib
+//
+//  Created by iroid on 17/05/21.
+//
+
+import Foundation
+
+
+public final class MirajLib {
+
+    let name = "MirajLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
