7669263249e35b6f6b13497d95eea8b98e505f31
miraj <miraj@iroidsolutions.com>
2021-05-17 10:50:57 +0530

    Initial commit

---
+++ MirajLib/MirajLib.h
@@ -0,0 +1,18 @@
+//
+//  MirajLib.h
+//  MirajLib
+//
+//  Created by iroid on 17/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MirajLib.
+FOUNDATION_EXPORT double MirajLibVersionNumber;
+
+//! Project version string for MirajLib.
+FOUNDATION_EXPORT const unsigned char MirajLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MirajLib/PublicHeader.h>
+
+
